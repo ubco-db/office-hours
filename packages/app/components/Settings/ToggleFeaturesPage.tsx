3c9aea5653b091cb7711e36399858609d898a4b6
Adam Fipke <adamfipke@gmail.com>
2024-02-04 22:01:09 -0800

    skeleton features page

--- packages/app/components/Settings/ToggleFeaturesPage.tsx
+++ packages/app/components/Settings/ToggleFeaturesPage.tsx
@@ -1,23 +1,46 @@
+import { Form, Checkbox } from 'antd'
 import { ReactElement } from 'react'
-import CourseOverrideSettings from './CourseOverrideSettings'
 import styled from 'styled-components'
-import CourseRoster from './CourseRoster'
 
-type CourseRosterPageProps = { courseId: number }
+type ToggleFeaturesPageProps = { courseId: number }
 
-const CourseRosterPageComponent = styled.div`
+const ToggleFeaturesPageComponent = styled.div`
   width: 90%;
   margin-left: auto;
   margin-right: auto;
   padding-top: 50px;
 `
 
+const CustomFormItem = styled(Form.Item)`
+  padding-bottom: 1rem;
+  margin-bottom: 1rem;
+
+  &:last-child {
+    padding-bottom: 0;
+    margin-bottom: 0;
+  }
+`
+
 export default function ToggleFeaturesPage({
   courseId,
-}: CourseRosterPageProps): ReactElement {
+}: ToggleFeaturesPageProps): ReactElement {
   return (
-    <CourseRosterPageComponent>
-      <h1>hello world!</h1>
-    </CourseRosterPageComponent>
+    <ToggleFeaturesPageComponent>
+      <h2>Enable/Disable Features for this Course</h2>
+      <Form className="ml-2 !text-2xl">
+        <CustomFormItem>
+          <Checkbox defaultChecked>Asynchronous Question Center</Checkbox>
+        </CustomFormItem>
+        <CustomFormItem>
+          <Checkbox defaultChecked>Chatbot</Checkbox>
+        </CustomFormItem>
+        <CustomFormItem>
+          <Checkbox defaultChecked>Queues</Checkbox>
+        </CustomFormItem>
+        <CustomFormItem>
+          <Checkbox defaultChecked>Ads</Checkbox>
+        </CustomFormItem>
+      </Form>
+    </ToggleFeaturesPageComponent>
   )
 }
